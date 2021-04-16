1e43463ef33b4eaff9a15d679b22dcd3b6de18ab
alex <alexander.nys@hotmail.com>
2021-04-16 17:14:17 +0200

    adding storyBoard and starting on the Model

---
+++ RecipleaseP10/RecipleaseP10/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  RecipleaseP10
+//
+//  Created by Alexandre NYS on 16/04/2021.
+//
+
+import Foundation
---
+++ RecipleaseP10/RecipleaseP10/Model/ApiKey.swift
@@ -0,0 +1,15 @@
+//
+//  ApiKeyManager.swift
+//  Reciplease
+//
+//  Created by Alexandre NYS on 16/04/2021.
+//
+
+
+import Foundation
+// Api key and ID for the recipe service.
+struct ApiKey {
+    static let id = "5e334cd4"
+    static let key = "5c619f1d12371d38505baa42aa7668b4"
+}
+
